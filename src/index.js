041da6a8967e5103514aac1665ed40d1744c54e8
unknown <evgen_abramovich@mail.ru>
2023-01-23 19:56:05 +0300

    исправление

--- src/index.js
+++ src/index.js
@@ -19,8 +19,8 @@ const element = <div className="footer">
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <ProductPage />
+        <App />
+        {/* <ProductPage /> */}
     </React.StrictMode>
 );
 
